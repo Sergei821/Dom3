56116efc9282feb6751911643a29bb29591f345f
Sergei821 <ps16111982@gmail.com>
2023-04-20 22:59:41 +0300

    zad3

---
+++ Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿//Задача 23
+//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+Console.Write("Введите положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Таблица кубов: ");
+for (int i = 1; i < number; i++)
+{
+    Console.Write(Math.Pow(i, 3) + ", ");    
+}
+Console.Write(Math.Pow(number, 3) + ";"); 
